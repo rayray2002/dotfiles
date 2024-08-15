003b980d0f4f0cc52e2f09a194ac5cce1d466f47
rayray2002 <rayray2002.huang@gmail.com>
2024-08-15 21:21:39 +0800

    init

---
+++ ssh/install.sh
@@ -0,0 +1,2 @@
+mkdir -p ~/.ssh
+cp -r $cwd/ssh/* ~/.ssh
---
+++ tmux/install.sh
@@ -0,0 +1,6 @@
+$cwd = $(pwd)
+
+cd $HOME
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp $cwd/.tmux.conf.local .
